3f2020c2c257aeb0f292d235379c9894e796eb3d
Thomas Boyle <tjb2597@rit.edu>
2014-03-02 20:08:19 -0500

    Mockup more or less done

---
+++ index.php
@@ -0,0 +1,191 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<title>SUPERCOOK</title>
+	<link rel="stylesheet" type="text/css" href="./assets/stylesheets/base.css"/>
+	<link rel="stylesheet" type="text/css" href="./assets/stylesheets/skeleton.css"/>
+	<link rel="stylesheet" type="text/css" href="./assets/stylesheets/layout.css"/>
+	<!--[if IE]>
+	  <link href="/stylesheets/ie.css" media="screen, projection" rel="stylesheet" type="text/css" />
+	<![endif]-->
+	<!-- <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,800' rel='stylesheet' type='text/css'> -->
+</head>
+<body>
+	<header>
+		<div class='container' id='header_container'>
+			<div class='sixteen columns'>
+				<h2 class='left'>SUPER<span>COOK</span></h2>
+				<div class='button-wrapper right'>
+					<button type='button'>LOGIN</button>
+					<button type='button'>SIGN UP</button>
+				</div>
+			</div>
+		</div>
+	</header>
+
+	<div class='container body'>
+
+		<aside class='four columns card'>
+			<div class='ingredients form-border'>				
+				<form>
+					<input type="text" value="Ingredient">
+				</form>
+				<ul class='ingred'>
+					<li>Lorem Ipsum doler il semit cum lade</li>
+					<li>Lorem Ipsum doler il semit cum lade</li>
+					<li>Lorem Ipsum doler il semit cum lade</li>
+					<li>Lorem Ipsum doler il semit cum lade</li>
+					<li>Lorem Ipsum doler il semit cum lade</li>
+					<li>Lorem Ipsum doler il semit cum lade</li>
+					<li>Lorem Ipsum doler il semit cum lade</li>
+					<a href="#">See 26 More</a>
+				</ul>
+			</div>		
+			<div class='prep form-border'>
+				<label>Prep time</label>
+				<input id="slider1" type="range" min="100" max="500" step="10" />
+			</div>
+			<div class='cook'>
+				<label>Cook time</label>
+				<input id="slider1" type="range" min="100" max="500" step="10" />
+			</div>
+		</aside>
+
+		<section class='twelve columns'>
+
+			<div class='meal'>
+				<button type='button' class='current'>ALL</button>
+				<button type='button'>BREAKFAST</button>
+				<button type='button'>LUNCH</button>
+				<button type='button'>DINNER</button>
+				<button type='button'>DESSERT</button>
+			</div>
+			<div class='recipes'>
+				<div class='four columns alpha recipe-column'>
+					<div class='card recipe'>
+						<img src="http://placehold.it/350x150" width="100%">
+						<h6>Recipe name</h6>
+						<div class='wrapper'>
+							<div class='iIcon'><p>7 ing</p></div>
+							<div class='pIcon'><p>20 min</p></div>
+							<div class='cIcon'><p>60 min</p></div>
+						</div>
+					</div>
+					<div class='card recipe'>
+						<img src="http://placehold.it/350x200" width="100%">
+						<h6>Recipe name</h6>
+						<div class='wrapper'>
+							<div class='iIcon'><p>7 ing</p></div>
+							<div class='pIcon'><p>20 min</p></div>
+							<div class='cIcon'><p>60 min</p></div>
+						</div>
+					</div>
+					<div class='card recipe'>
+						<img src="http://placehold.it/350x400" width="100%">
+						<h6>Recipe name</h6>
+						<div class='wrapper'>
+							<div class='iIcon'><p>7 ing</p></div>
+							<div class='pIcon'><p>20 min</p></div>
+							<div class='cIcon'><p>60 min</p></div>
+						</div>
+					</div>
+					<div class='card recipe'>
+						<img src="http://placehold.it/350x300" width="100%">
+						<h6>Recipe name</h6>
+						<div class='wrapper'>
+							<div class='iIcon'><p>7 ing</p></div>
+							<div class='pIcon'><p>20 min</p></div>
+							<div class='cIcon'><p>60 min</p></div>
+						</div>
+					</div>
+				</div>
+				<div class='four columns middle recipe-column'>
+				    <div class='card recipe'>
+						<img src="http://placehold.it/350x200" width="100%">
+						<h6>Recipe name</h6>
+						<div class='wrapper'>
+							<div class='iIcon'><p>7 ing</p></div>
+							<div class='pIcon'><p>20 min</p></div>
+							<div class='cIcon'><p>60 min</p></div>
+						</div>
+					</div>
+					<div class='card recipe'>
+						<img src="http://placehold.it/350x400" width="100%">
+						<h6>Recipe name</h6>
+						<div class='wrapper'>
+							<div class='iIcon'><p>7 ing</p></div>
+							<div class='pIcon'><p>20 min</p></div>
+							<div class='cIcon'><p>60 min</p></div>
+						</div>
+					</div>
+					<div class='card recipe'>
+						<img src="http://placehold.it/350x150" width="100%">
+						<h6>Recipe name</h6>
+						<div class='wrapper'>
+							<div class='iIcon'><p>7 ing</p></div>
+							<div class='pIcon'><p>20 min</p></div>
+							<div class='cIcon'><p>60 min</p></div>
+						</div>
+					</div>
+					<div class='card recipe'>
+						<img src="http://placehold.it/350x250" width="100%">
+						<h6>Recipe name</h6>
+						<div class='wrapper'>
+							<div class='iIcon'><p>7 ing</p></div>
+							<div class='pIcon'><p>20 min</p></div>
+							<div class='cIcon'><p>60 min</p></div>
+						</div>
+					</div>
+				</div>
+				<div class='four columns omega recipe-column'>
+					<div class='card recipe'>
+						<img src="http://placehold.it/350x400" width="100%">
+						<h6>Recipe name</h6>
+						<div class='wrapper'>
+							<div class='iIcon'><p>7 ing</p></div>
+							<div class='pIcon'><p>20 min</p></div>
+							<div class='cIcon'><p>60 min</p></div>
+						</div>
+					</div>
+					<div class='card recipe'>
+						<img src="http://placehold.it/350x150" width="100%">
+						<h6>Recipe name</h6>
+						<div class='wrapper'>
+							<div class='iIcon'><p>7 ing</p></div>
+							<div class='pIcon'><p>20 min</p></div>
+							<div class='cIcon'><p>60 min</p></div>
+						</div>
+					</div>
+					<div class='card recipe'>
+						<img src="http://placehold.it/350x200" width="100%">
+						<h6>Recipe name</h6>
+						<div class='wrapper'>
+							<div class='iIcon'><p>7 ing</p></div>
+							<div class='pIcon'><p>20 min</p></div>
+							<div class='cIcon'><p>60 min</p></div>
+						</div>
+					</div>
+					<div class='card recipe'>
+						<img src="http://placehold.it/350x125" width="100%">
+						<h6>Recipe name</h6>
+						<div class='wrapper'>
+							<div class='iIcon'><p>7 ing</p></div>
+							<div class='pIcon'><p>20 min</p></div>
+							<div class='cIcon'><p>60 min</p></div>
+						</div>
+					</div> 
+				</div>
+
+			</div>
+
+		</section>
+
+	</div>
+
+	<footer>
+
+
+	</footer>
+</body>
+</html>
